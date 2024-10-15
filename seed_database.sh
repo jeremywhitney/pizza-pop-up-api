8f0a8b57676fd37ee4152f3d53a52310a345de28
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-15 15:33:59 -0500

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./pizzaapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations pizzaapi
+python3 manage.py migrate pizzaapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
