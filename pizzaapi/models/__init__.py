42d8cd0d6a958c15ffdcc7c259df072360f20a7f
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-16 14:28:31 -0500

    created 'OrderProduct' model and updated imports

--- pizzaapi/models/__init__.py
+++ pizzaapi/models/__init__.py
@@ -1,6 +1,7 @@
 from .category import Category
 from .employee_profile import EmployeeProfile
 from .order import Order
+from .order_product import OrderProduct
 from .payment import Payment
 from .product import Product
 from .profile import Profile
