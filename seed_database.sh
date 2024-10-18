15660732c8e52de82000b99255ed82f6d928c664
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-18 13:48:45 -0500

    created category fixture and seeded database

--- seed_database.sh
+++ seed_database.sh
@@ -5,6 +5,5 @@ rm -rf ./pizzaapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations pizzaapi
 python3 manage.py migrate pizzaapi
-python3 manage.py loaddata users
-python3 manage.py loaddata tokens
+python3 manage.py loaddata categories
 
