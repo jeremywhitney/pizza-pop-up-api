a7d4a07324cad40444a8756ec2064773109cb830
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-18 19:42:29 -0500

    Created fixtures for 'orders', 'orderproducts', 'pizzatoppings'

--- seed_database.sh
+++ seed_database.sh
@@ -8,4 +8,7 @@ python3 manage.py migrate pizzaapi
 python3 manage.py loaddata categories
 python3 manage.py loaddata payments
 python3 manage.py loaddata products
+python3 manage.py loaddata orders
+python3 manage.py loaddata orderproducts
+python3 manage.py loaddata pizzatoppings
 
