1d1e32bec019551ad39d340a63c5af9e3efedef7
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-19 21:58:40 -0500

    Re-seeded database with updated fixtures. Pizzas with toppings now only display the toppings associated with that order, not all toppings.

--- seed_database.sh
+++ seed_database.sh
@@ -8,4 +8,7 @@ python3 manage.py migrate pizzaapi
 python3 manage.py migrate categories
 python3 manage.py migrate payments
 python3 manage.py migrate products
+python3 manage.py migrate orders
+python3 manage.py migrate orderproducts
+python3 manage.py migrate pizzatoppings
 
