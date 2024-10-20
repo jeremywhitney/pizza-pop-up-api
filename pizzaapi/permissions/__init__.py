3409316154bff5b78b4bb947b27c678cf863a3a3
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-20 15:47:24 -0500

    Updated user permissions for viewing orders. Generated token for superuser.

---
+++ pizzaapi/permissions/__init__.py
@@ -0,0 +1,1 @@
+from .order_permissions import OrderPermission
--- pizzaapi/views/order_viewset.py
+++ pizzaapi/views/order_viewset.py
@@ -2,17 +2,22 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from ..models.order import Order
+from ..permissions.order_permissions import OrderPermission
 from ..serializers.order_serializer import OrderSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [OrderPermission]
 
     # Override get_queryset to filter by cart or orders
     def get_queryset(self):
         user = self.request.user
-        # Filter orders by customer user and return all orders (cart + completed)
+        # Superusers and staff can see all orders
+        if user.is_superuser or user.is_staff:
+            return Order.objects.all()
+        # Regular users only see their orders
         return Order.objects.filter(customer__user=user)
 
     # Action to fetch cart items only (PENDING orders with no payment)
