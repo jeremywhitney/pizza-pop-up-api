ddef97cbd36cc14f500b2f511ee5f583ce3cbfa8
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-21 09:35:45 -0500

    Updated permissions for 'payments'

--- pizzaapi/views/payment_viewset.py
+++ pizzaapi/views/payment_viewset.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
 from ..models.payment import Payment
+# from ..permissions.order_permissions import OrderPermission
 from ..serializers.payment_serializer import PaymentSerializer
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    # permission_classes = [OrderPermission]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Superusers and staff can see all payment methods
+        if user.is_superuser or user.is_staff:
+            return Payment.objects.all()
+        # Regular users only see their payment methods
+        return Payment.objects.filter(customer__user=user)
