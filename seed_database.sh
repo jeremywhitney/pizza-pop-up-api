2672cd2d779a3a8cbb47959e6acdd406646ece6a
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-22 09:48:14 -0500

    Added user, token, employeeprofile, profile fixtures, updated seed_database.sh, and added a fixture validation file

--- seed_database.sh
+++ seed_database.sh
@@ -5,10 +5,13 @@ rm -rf ./pizzaapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations pizzaapi
 python3 manage.py migrate pizzaapi
+python3 manage.py migrate users
+python3 manage.py migrate tokens
+python3 manage.py migrate profiles
+python3 manage.py migrate employee_profiles
 python3 manage.py migrate categories
 python3 manage.py migrate payments
 python3 manage.py migrate products
 python3 manage.py migrate orders
-python3 manage.py migrate orderproducts
-python3 manage.py migrate pizzatoppings
-
+python3 manage.py migrate order_products
+python3 manage.py migrate pizza_toppings
