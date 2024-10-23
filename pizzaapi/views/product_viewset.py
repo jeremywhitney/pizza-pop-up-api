d77db46727356df184a0f14e03732199e3f5ff4f
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-23 09:54:07 -0500

    updated permissions for 'products' so any user can view

--- pizzaapi/views/product_viewset.py
+++ pizzaapi/views/product_viewset.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from ..models.product import Product
 from ..serializers.product_serializer import ProductSerializer
 
@@ -6,3 +7,4 @@ from ..serializers.product_serializer import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
