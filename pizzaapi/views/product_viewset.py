4941cfffeaa2f4c29fb4b3047dff33fd9132d27b
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-29 09:38:06 -0500

    Backend enhancements for employee user considerations, specifically creating, editing, deleting products, and making employee/customer-specific responses from /profile

--- pizzaapi/views/product_viewset.py
+++ pizzaapi/views/product_viewset.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from ..models.product import Product
 from ..serializers.product_serializer import ProductSerializer
 
@@ -6,3 +7,27 @@ from ..serializers.product_serializer import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return Response(
+                {"error": "Only staff can create products"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return Response(
+                {"error": "Only staff can update products"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return Response(
+                {"error": "Only staff can delete products"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().destroy(request, *args, **kwargs)
--- pizzaproject/urls.py
+++ pizzaproject/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from pizzaapi.views import *
