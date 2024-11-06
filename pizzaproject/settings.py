6ac43c7db12e37c72de952c1dd2093f4261a9fae
Jeremy Whitney <bearwhitness@gmail.com>
2024-11-06 16:26:59 -0600

    Deployment changes 2

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -49,7 +49,7 @@ CORS_ORIGIN_WHITELIST = (
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://urchin-app-2pbbs.ondigitalocean.app/",
+    "https://urchin-app-2pbbs.ondigitalocean.app",
 )
 
 
