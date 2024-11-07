f3916a8e6d2cf290ad8908728275392c4d338772
Jeremy Whitney <bearwhitness@gmail.com>
2024-11-06 22:16:57 -0600

    Removed load_dotenv() from settings.py and set environment variables directly in DO

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
